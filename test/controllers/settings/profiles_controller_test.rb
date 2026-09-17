910dc6dd525b37c20adca4c00f79ed95ecc477b2
Grzegorz Malinowski <git.15m27@passmail.net>
2026-09-17 14:17:58 +0200

    feat: add basic profile page

---
+++ test/controllers/settings/profiles_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class Settings::ProfilesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "redirects unauthenticated user to sign in" do
+    get edit_settings_profile_path
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "authenticated user can access profile edit page" do
+    sign_in users(:one)
+    get edit_settings_profile_path
+    assert_response :success
+  end
+end
