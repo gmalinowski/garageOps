4c94983edd7d52de33c77deecbc7311b04e8c76a
Grzegorz Malinowski <git.15m27@passmail.net>
2026-09-19 22:58:53 +0200

    feat: redirect authenticated users to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
