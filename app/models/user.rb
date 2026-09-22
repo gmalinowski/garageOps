18f643fe552dd10cb24fabedc2487d90fcbcdd27
Grzegorz Malinowski <git.15m27@passmail.net>
2026-09-22 21:09:01 +0200

    feat: add sign up CTA button to landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :registerable
 
   has_many :memberships, dependent: :restrict_with_error
   has_many :organizations, through: :memberships
